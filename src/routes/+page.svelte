baa41d22749b077b75656f31abb8beec91a4980e
Kozbishoww <118129067+Kozbishoww@users.noreply.github.com>
2023-04-13 20:36:34 +0300

    Kisiselwebo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,63 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Benim Kişisel Web Sitem</title>
-  <link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<title>Ozan Çalışkan Kişisel Web Sitesi</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <header>
-    <nav>
-      <ul>
-        <li><a href="#anasayfa">Anasayfa</a></li>
-        <li><a href="#hakkimda">Hakkımda</a></li>
-        <li><a href="#projelerim">Projelerim</a></li>
-        <li><a href="#iletisim">İletişim</a></li>
-      </ul>
-    </nav>
-  </header>
-  <main>
-    <section id="anasayfa">
-      <h1>Benim Kişisel Web Sitem</h1>
-      <p>Merhaba, benim adım Ozan ve bu benim kişisel web sitem. Sitemdeki içerikler hakkında daha fazla bilgi edinmek için aşağıdaki menüyü kullanabilirsiniz.</p>
-    </section>
-    <section id="hakkimda">
-      <h2>Hakkımda</h2>
-      <p>Ben Ozan, bir yazılım geliştiricisiyim. Uzun yıllardır bu alanda çalışıyorum ve birçok projeye imza attım. Sitemdeki projelerim bölümünden daha fazla bilgi edinebilirsiniz.</p>
-    </section>
-    <section id="projelerim">
-      <h2>Projelerim</h2>
-      <ul>
-        <li>
-          <h3>Proje 1</h3>
-          <p>Açıklama</p>
-          <a href="#">Daha fazla bilgi</a>
-        </li>
-        <li>
-          <h3>Proje 2</h3>
-          <p>Açıklama</p>
-          <a href="#">Daha fazla bilgi</a>
-        </li>
-      </ul>
-    </section>
-    <section id="iletisim">
-      <h2>İletişim</h2>
-      <form>
-        <label for="name">Adınız:</label>
-        <input type="text" id="name" name="name" required>
-        <label for="email">E-posta adresiniz:</label>
-        <input type="email" id="email" name="email" required>
-        <label for="message">Mesajınız:</label>
-        <textarea id="message" name="message" required></textarea>
-        <button type="submit">Gönder</button>
-      </form>
-    </section>
-  </main>
-</body>
-</html>
 
+	<header>
+		<div class="container">
+			<h1 class="logo">Ozan Çalışkan</h1>
+			<nav>
+				<ul>
+					<li><a href="#">Anasayfa</a></li>
+					<li><a href="#">Hakkında</a></li>
+					<li><a href="#">Beceriler</a></li>
+					<li><a href="#">Portfolyo</a></li>
+					<li><a href="#">İletişim</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<section class="hero">
+		<div class="container">
+			<h1>Merhaba, ben Ozan Çalışkan.</h1>
+			<p>Web tasarımcısı ve geliştiriciyim. Kendimi sürekli geliştirmeye çalışıyorum ve en son teknolojileri kullanarak müşterilerim için özelleştirilmiş web siteleri oluşturuyorum.</p>
+		</div>
+	</section>
+
+	<section class="about">
+		<div class="container">
+			<h2>Hakkımda</h2>
+			<p>Ben Ozan Çalışkan, 1990 İstanbul doğumluyum. 2012 yılında Marmara Üniversitesi'nden mezun oldum ve web tasarımı ve geliştirme konusunda kendimi geliştirmeye başladım. Şu anda bağımsız bir web tasarımcısı ve geliştiricisi olarak çalışıyorum.</p>
+		</div>
+	</section>
+
+	<section class="skills">
+		<div class="container">
+			<h2>Becerilerim</h2>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>Javascript</li>
+				<li>jQuery</li>
+				<li>Bootstrap</li>
+				<li>WordPress</li>
+				<li>PHP</li>
+				<li>MySQL</li>
+			</ul>
+		</div>
+	</section>
+
+	<section class="portfolio">
+		<div class="container">
+			<h2>Portfolyo</h2>
+			<div class="project">
+				<img src="project1.jpg" alt="Proje 1">
+				<h3>Proje 1</h3>
+				<p>Bu proje için müşterimin ihtiyaçlarını analiz ettim
